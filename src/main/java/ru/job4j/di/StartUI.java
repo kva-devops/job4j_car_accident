d2da7a1d65c275e3d3d6de32e6af56f44f6de3dd
vladimirKutyavin <kva.devops@gmail.com>
2021-10-31 11:32:55 +0400

    2. Сканирование проекта [#1070 #222747] - correct

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,15 +1,18 @@
 package ru.job4j.di;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartUI {
 
-    @Autowired
     private Store store;
 
-    private ConsoleInput input = new ConsoleInput();
+    private ConsoleInput input;
+
+    public StartUI(Store store, ConsoleInput input) {
+        this.store = store;
+        this.input = input;
+    }
 
     public void add() {
         store.add(input.askStr());
