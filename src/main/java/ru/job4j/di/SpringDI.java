93b73d7a97fb2beca502698b006bb514b7aae551
vladimirKutyavin <kva.devops@gmail.com>
2021-10-31 10:18:13 +0400

    1. Spring DI. Запуск [#2091]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,16 @@
+package ru.job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add();
+        ui.add();
+        ui.print();
+    }
+}
